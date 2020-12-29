809271082b563bf07e8cc8cdb1f219d7797ef71a
joaquinLR10 <joaquin.larranaga@uabc.edu.mx>
2020-12-28 21:38:24 -0800

    COMANDO FUNCIONANDO

---
+++ Proyecto Diseño/Assets/Watson/Examples/VoiceCommandProcessor.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IBM.Watsson.Examples {
+
+    public class VoiceCommandProcessor : MonoBehaviour
+    {
+        static protected VoiceCommandProcessor s_VoiceInstance;
+        static public VoiceCommandProcessor Instance { get { return s_VoiceInstance; } }
+
+        public delegate void OnVoiceCommand(string action);
+        public OnVoiceCommand onVoiceCommand;
+
+        public List<string> actions;
+        public List<string> specialActions;
+
+        public List<GameObject> spawningPrefabs;
+
+
+        void Awake()
+        {
+            s_VoiceInstance = this;
+        }
+
+        public void Create(string transcript)
+        {
+            string[] words = transcript.Split(' ');
+            foreach (var word in words)
+            {
+                if (actions.Contains(word.ToLower()))
+                {
+                    if (onVoiceCommand != null)
+                    {
+                        onVoiceCommand.Invoke(word.ToLower());
+                    }
+                    return;
+                }
+            }
+
+            foreach (var word in words)
+            {
+                if (specialActions.Contains(word.ToLower()))
+                {
+                   if (word == "destroyed")
+                   {
+                       SpawnObject(words);
+                   }
+                   if (word == "instantiate")
+                   {
+                        
+                   }
+                }
+                break;
+            }
+        }
+
+        void SpawnObject(string[] words)
+        {
+            foreach (var word in words)
+            {
+                foreach (var prefab in spawningPrefabs)
+                {
+                    if (prefab.name == word.ToLower())
+                    {
+                      
+                         Destroy(prefab);
+                      
+                         //Instantiate(prefab, new Vector3(2f,0f,0f), Quaternion.identity);
+                        
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Proyecto Diseño/Assets/scripts/door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : VoiceInteractable
+{
+
+  
+  
+
+   
+
+    public override void VoiceInteract(string action)
+       {
+          
+        if(action=="open")
+        {
+        Destroy(gameObject);
+        }
+       
+
+       }
+      
+       
+        
+    }
